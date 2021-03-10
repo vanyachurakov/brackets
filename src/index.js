23ed397ea56016f981b87858703c713cfaf71566
Ivan Churakou <ccchurakovvv@gmail.com>
2021-03-10 10:06:02 +0300

    Implement function check(str, bracketsConfig)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let strArr = str.split("");
+    let isBracketsSplice = true;
+    while (isBracketsSplice) {
+        isBracketsSplice = false;
+        for (let i = 0; i < strArr.length; i++) {
+            bracketsConfig.forEach((element) => {
+                if (
+                    strArr.indexOf(element[0], i) != -1 &&
+                    strArr.indexOf(element[1], i + 1) ==
+                        strArr.indexOf(element[0], i) + 1
+                ) {
+                    strArr.splice(strArr.indexOf(element[0], i), 2);
+                    i -= 2;
+                    let isBracketsSplice = true;
+                }
+            });
+        }
+    }
+    return strArr.length == 0 ? true : false;
+};
+
+// console.log(
+//     check("((()))()", [
+//         ["(", ")"],
+//         ["[", "]"],
+//     ])
+// );
